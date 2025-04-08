59acd72c4b185684a6d7473c7198d943ae22a442
Haneul Lee <145983374+tishakong@users.noreply.github.com>
2025-04-08 15:33:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long n;
+    cin >> n;
+
+    cout << n*(n-1)/2 << '\n';
+    cout << 2;
+
+    return 0;   
+}
