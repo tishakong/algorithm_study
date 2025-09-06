cf6ab57b482247be13b03f9eb94ee86ae2c170c1
Haneul Lee <145983374+tishakong@users.noreply.github.com>
2025-09-06 14:37:35 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,55 +1,53 @@
-#include <iostream>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
 int N;
-int arr[11], op[4];          // arr: 숫자 배열, op: +, -, *, / 개수
-int maxResult = INT_MIN;
-int minResult = INT_MAX;
+int arr[11], op[4];
+long long maxResult, minResult;
 
-void dfs(int idx, int current) {
+void dfs(int idx, long long current) {
     if (idx == N) {
-        // 모든 수를 다 사용했을 때 결과 갱신
-        maxResult = max(maxResult, current);
-        minResult = min(minResult, current);
+        if (current > maxResult) maxResult = current;
+        if (current < minResult) minResult = current;
         return;
     }
-    // 덧셈
     if (op[0] > 0) {
         op[0]--;
         dfs(idx+1, current + arr[idx]);
         op[0]++;
     }
-    // 뺄셈
     if (op[1] > 0) {
         op[1]--;
         dfs(idx+1, current - arr[idx]);
         op[1]++;
     }
-    // 곱셈
     if (op[2] > 0) {
         op[2]--;
         dfs(idx+1, current * arr[idx]);
         op[2]++;
     }
-    // 나눗셈 (C++14 기준: 음수도 절댓값으로 나눈 몫을 취하고, 결과에 부호를 붙인다)
     if (op[3] > 0) {
         op[3]--;
-        dfs(idx+1, current / arr[idx]);
+        long long next;
+        // 명시적 음수 처리 (C++ 표준과 일치: trunc toward zero)
+        if (current < 0) next = - ( ( -current ) / arr[idx] );
+        else next = current / arr[idx];
+        dfs(idx+1, next);
         op[3]++;
     }
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> N;
-    for(int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
-    for(int i = 0; i < 4; i++) {
-        cin >> op[i];
-    }
-    // 첫 번째 숫자를 시작값으로 하여 dfs 호출
+    for (int i = 0; i < N; ++i) cin >> arr[i];
+    for (int i = 0; i < 4; ++i) cin >> op[i];
+
+    maxResult = LLONG_MIN;
+    minResult = LLONG_MAX;
     dfs(1, arr[0]);
-    cout << maxResult << "\n" << minResult << endl;
+    cout << maxResult << '\n' << minResult << '\n';
     return 0;
 }
